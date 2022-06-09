e1cfb4e6e84837476133ec2e47820d39d59eda03
Lirac <Liraclomex@yahoo.com>
2022-06-09 12:20:13 +0100

    Adds movie trailer

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import axios from '../services/axios'
+import Youtube from 'react-youtube'
+import movieTrailer from 'movie-trailer'
 
 const base_url = 'https://image.tmdb.org/t/p/original/'
 
 const Row = ({ title, fetchUrl }) => {
   const [movies, setMovies] = useState([])
+  const [trailerUrl, setTrailerUrl] = useState('')
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl)
@@ -13,6 +16,27 @@ const Row = ({ title, fetchUrl }) => {
     }
     fetchData()
   }, [fetchUrl])
+
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+    },
+  }
+
+  const handleHover = movie => {
+    if (trailerUrl) {
+      setTrailerUrl('')
+    } else {
+      movieTrailer(movie?.name || '')
+        .then(url => {
+          const urlParams = new URLSearchParams(new URL(url).search)
+          setTrailerUrl(urlParams.get('v'))
+        })
+        .catch(error => console.log(error))
+    }
+  }
   return (
     <div>
       <h2 className="font-bold text-xl text-white">{title}</h2>
@@ -23,10 +47,12 @@ const Row = ({ title, fetchUrl }) => {
             key={movie.id}
             src={`${base_url}${movie.poster_path}`}
             alt={movie.name}
-            className="w-96 h-40 object-fill z-0 cursor-pointer transition-all ease-in delay-300 duration-300 mr-1 hover:scale-150 hover:z-20 rounded-sm"
+            onClick={() => handleHover(movie)}
+            className="relative w-96 h-40 object-fill z-0 cursor-pointer transition-all ease-in delay-300 duration-300 mr-1 hover:scale-150 hover:z-20 rounded-sm"
           />
         ))}
       </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
   )
 }
