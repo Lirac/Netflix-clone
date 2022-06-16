e82fa9dd078eb8c3a3f1a267d21e30a6e08bd509
Lirac <Liraclomex@yahoo.com>
2022-06-16 11:22:07 +0100

    Fixes footer responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { IconContext } from 'react-icons'
 
 const footer = () => {
   return (
-    <div className="pt-20 pb-4 mx-auto w-[60%]">
+    <div className="pt-20 pb-4 mx-auto w-[92%] xl:w-[60%]">
       <div className="flex gap-6">
         <IconContext.Provider value={{ color: 'white', size: '25' }}>
           <FaFacebookF className="cursor-pointer" />
@@ -11,44 +11,44 @@ const footer = () => {
           <FaYoutube className="cursor-pointer" />
         </IconContext.Provider>
       </div>
-      <div className="text-gray-200/60 text-xs flex flex-wrap gap-x-24 gap-y-4 mt-4 font-medium">
-        <a href="q" className="basis-[16%] hover:underline">
+      <div className="text-gray-200/60 text-xs flex flex-wrap gap-x-16 md:gap-x-24 gap-y-4 mt-4 lg:font-medium">
+        <a href="q" className="basis-[35%] md:basis-[16%] hover:underline">
           Audio and Subtitles
         </a>
-        <a href="q" className="basis-[16%] hover:underline">
+        <a href="q" className=" basis-[35%] md:basis-[16%] hover:underline">
           Audio Description
         </a>
-        <a href="q" className="basis-[16%] hover:underline">
+        <a href="q" className=" basis-[35%] md:basis-[16%] hover:underline">
           Help Center
         </a>
-        <a href="q" className="basis-[16%] hover:underline">
+        <a href="q" className=" basis-[35%] md:basis-[16%] hover:underline">
           Gift Cards
         </a>
-        <a href="q" className="basis-[16%] hover:underline">
+        <a href="q" className=" basis-[35%] md:basis-[16%] hover:underline">
           Media Center
         </a>
-        <a href="q" className="basis-[16%] hover:underline">
+        <a href="q" className=" basis-[35%] md:basis-[16%] hover:underline">
           Investor Relations
         </a>
-        <a href="q" className="basis-[16%] hover:underline">
+        <a href="q" className=" basis-[35%] md:basis-[16%] hover:underline">
           Jobs
         </a>
-        <a href="q" className="basis-[16%] hover:underline">
+        <a href="q" className=" basis-[35%] md:basis-[16%] hover:underline">
           Terms of Use
         </a>
-        <a href="q" className="basis-[16%] hover:underline">
+        <a href="q" className=" basis-[35%] md:basis-[16%] hover:underline">
           Privacy
         </a>
-        <a href="q" className="basis-[16%] hover:underline">
+        <a href="q" className=" basis-[35%] md:basis-[16%] hover:underline">
           Legal Notices
         </a>
-        <a href="q" className="basis-[16%] hover:underline">
+        <a href="q" className="basis-[35%] md:basis-[16%] hover:underline">
           Cookie Preference
         </a>
-        <a href="q" className="basis-[16%] hover:underline">
+        <a href="q" className="basis-[35%] md:basis-[16%] hover:underline">
           Corporate Information
         </a>
-        <a href="q" className="basis-[16%] hover:underline">
+        <a href="q" className="basis-[35%] md:basis-[16%] hover:underline">
           Contact Us
         </a>
       </div>
